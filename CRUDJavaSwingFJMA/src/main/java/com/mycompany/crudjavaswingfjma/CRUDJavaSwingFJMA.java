b96ffe4cd700b39beee1dcd3b1b860294cba90d8
Fernando Jose Majano Aguirre <fernando13522477@outlook.com>
2024-06-12 10:05:03 -0600

    first commit

---
+++ CRUDJavaSwingFJMA/src/main/java/com/mycompany/crudjavaswingfjma/CRUDJavaSwingFJMA.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.crudjavaswingfjma;
+
+/**
+ *
+ * @author Fernando
+ */
+public class CRUDJavaSwingFJMA {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
